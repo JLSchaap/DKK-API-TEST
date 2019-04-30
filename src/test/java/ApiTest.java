bb8b300fcfff345bf118daac6f205ac793ebe5ab
Schaap <schaaj@kadaster.local>
2019-04-30 16:58:55 +0200

    Run tests in parallel

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,18 +1,21 @@
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-
+/* 
 @RunWith(Karate.class)
+//@CucumberOptions(tags = "~@bug")
 public class ApiTest {
+} */
+
+public class ApiTest {
+
+    @Test
+    public void testParallel() {
+        Results results = Runner.parallel(getClass(), 20, "target/surefire-reports");
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    }
+
 }
-
-//public class ApiTest {
-
-//    @Test
-//    public void testParallel() {
-//   Results results = Runner.parallel(getClass(), 5, "target/surefire-reports");
-//        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
-//    }
-
-//}
