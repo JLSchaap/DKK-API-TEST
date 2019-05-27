06eb35393f7e3381840acbdd96cd85bbda655484
Schaap <schaaj@kadaster.local>
2019-05-27 15:38:23 +0200

    Enviroment var added for baseurl

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,7 +1,11 @@
 function() {
- // karate.configure('connectTimeout', 5000);
- // karate.configure('readTimeout', 5000);
-  var config = { apiBaseUrl: 'https://download.pdok.io/kadaster/dkk/api/v1' };
+  // karate.configure('connectTimeout', 5000);
+  // karate.configure('readTimeout', 5000);
+  baseurl = java.lang.System.getenv('apiBaseUrl'); 
+  if (!baseurl) {
+    baseurl = 'https://download.pdok.io/kadaster/dkk/api/v1' ; 
+  }
+  var config = { apiBaseUrl: baseurl };
   karate.log('base url :', config);
   return config;
 }
