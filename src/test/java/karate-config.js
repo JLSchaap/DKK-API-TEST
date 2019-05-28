722079e8cd9ece7977e30b781f61a642bc6cbecd
John Schaap <JLSchaap@users.noreply.github.com>
2019-05-28 11:56:01 +0200

    ssl config

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,7 +1,7 @@
 function() {
   // karate.configure('connectTimeout', 5000);
   // karate.configure('readTimeout', 5000);
-  karate.configure('ssl', true);
+  // karate.configure('ssl', true);
   baseurl = java.lang.System.getenv('apiBaseUrl');
   if (!baseurl) {
       baseurl = 'https://download.pdok.io/' ; 
