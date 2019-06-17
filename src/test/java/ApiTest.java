39dbfe6e3eb81adbb942c7f33eb29d7adb2ff4d6
Schaap <john.schaap@kadaster.nl>
2019-06-17 15:40:26 +0200

    Open api changed

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -8,22 +8,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-
-
+/*
 @RunWith(Karate.class)
 @CucumberOptions(tags = {"~@TEMPLATE","~SMOKE","~BUG","@WIPTEST"})
 public class ApiTest {
 } 
-/*
+*/
+
+@CucumberOptions(tags = { "SMOKE", "~@TEMPLATE", "~WIP", "~BUG", "~@WIPTEST" })
 
 public class ApiTest {
 
     @Test
     public void testParallel() {
         Results results = Runner.parallel(getClass(), 20, "target/surefire-reports");
-  
+
         assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
     }
 
 }
-*/ 
--- src/test/java/download/T01_Openapi.feature
+++ src/test/java/download/T01_Openapi.feature
@@ -1,4 +1,4 @@
-@SMOKE
+@WIP
 Feature: DKK API heeft open api configuratie
   de API wordt beschikbaargesteld d.m.v. een openapi spec
   Background:
